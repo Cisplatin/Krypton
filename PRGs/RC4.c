99a460b73f095f6bc6732f13aaa80fe3e9dab6e9
Xenonstory <simon.j.hajjar@gmail.com>
2015-06-09 16:55:33 -0400

    found append error to fix later

--- PRGs/RC4.c
+++ PRGs/RC4.c
@@ -1,9 +1,11 @@
 /* RC4.c - Written by Xenonstory on June 8, 2015 *
  * An implementation of the RC4 PRNG                    */
 
-#include "OneTimePad.h"
+#include "RC4.h"
 #include <assert.h>
 #include <stdlib.h>
+
+#include <stdio.h>
 
 // See OneTimePad.h for details
 BinStr RC4(BinStr key, int n) {
@@ -14,7 +16,8 @@ BinStr RC4(BinStr key, int n) {
 	}
 	int j = 0;
 	for(int i = 0; i < 255; i++) {
-		j = (j + S[i] + toDecimal(key->getByte(i % bytes(key)))) % 256;
+		int a = toDecimal(getByte(key, i % bytes(key)));
+                j = (j + S[i] + a) % 256;
 		int buffer = S[i];
 		S[i] = S[j];
 		S[j] = buffer;
@@ -22,14 +25,18 @@ BinStr RC4(BinStr key, int n) {
 
 	// End with the pseudo-random generation algorithm
 	int k = 0, f = 0;
-	BinStr new = empty_BinStr(n);
-	for(int m = 0; m < n; m++) {
+	BinStr new = create_BinStr("", 0);
+	for(int m = 0; m < 1 + n / 8; m++) {
 		k = (k + 1) % 256;
 		j = (j + S[k]) % 256;
 		int buffer = S[k];
 		S[k] = S[j];
 		S[j] = buffer;
-		app = S[(S[i] + S[j]) % 256];
-	}
+		BinStr app = int_to_BinStr(S[(S[k] + S[j]) % 256]);
+                replace(new, append(new, app));
+                print(new); printf("\n");
+        }
+        print(new); printf("\n");
+        replace(new, cut(new, n));
 	return new;
 }
--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include "Structures/BinStr.h"
+#include "PRGs/RC4.h"
 
 int main() {
-	print(int_to_BinStr(102));
+	BinStr a = create_BinStr("10111111011", 11);
+        print(RC4(a, 15));
 	return 0;
 }
