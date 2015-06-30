5b9b093f8984725bc08bbc6e4ddd6e171fd9f207
Xenonstory <simon.j.hajjar@gmail.com>
2015-06-30 17:54:47 -0400

    Added final permutation, cleaned up code

--- Ciphers/DES.c
+++ Ciphers/DES.c
@@ -5,37 +5,51 @@
 #include <assert.h>
 #include <stdlib.h>
 
+int permutation[] = {58, 50, 42, 34, 26, 18, 10, 2,
+                     60, 52, 44, 36, 28, 20, 12, 4,
+                     62, 54, 46, 38, 30, 22, 14, 6,
+                     64, 56, 48, 40, 32, 24, 16, 8,
+                     57, 49, 41, 33, 25, 17,  9, 1,
+                     59, 51, 43, 35, 27, 19, 11, 3,
+                     61, 53, 45, 37, 29, 21, 13, 5,
+                     63, 55, 47, 39, 31, 23, 15, 7};
+
 // initialPermutation(block) returns a new BinStr in the form of the initial
 //   permutation for the DES cipher
 // effects: allocates memory to a new BinStr
-// requires: block is a valid BinStr and block->length == 64
+// requires: block is a valid BinStr and block->length == DES_BLOCK_SIZE
 BinStr initialPermutation(BinStr block) {
-	assert(block != NULL && block->length == 64);
-	int permutation[64] = {58, 50, 42, 34, 26, 18, 10, 2,
-                               60, 52, 44, 36, 28, 20, 12, 4,
-                               62, 54, 46, 38, 30, 22, 14, 6,
-                               64, 56, 48, 40, 32, 24, 16, 8,
-                               57, 49, 41, 33, 25, 17,  9, 1,
-                               59, 51, 43, 35, 27, 19, 11, 3,
-                               61, 53, 45, 37, 29, 21, 13, 5,
-                               63, 55, 47, 39, 31, 23, 15, 7};
-	BinStr new = empty_BinStr(64);
+	assert(block != NULL && block->length == DES_BLOCK_SIZE);
+	BinStr new = empty_BinStr(DES_BLOCK_SIZE);
 	for(int i = 0; i < 64; i++) {
 		new->bits[i] = block->bits[permutation[i]];
 	}
 	return new;
 }
 
+// finalPermutation(block) returns a new BinStr in the form of the final
+//   permutation for the DES cipher
+// effects: allocates memory to a new BinStr
+// requires: block is a valid BinStr and block->length == DES_BLOCK_SIZE
+BinStr initialPermutation(BinStr block) {
+        assert(block != NULL && block->length == DES_BLOCK_SIZE);
+        BinStr new = empty_BinStr(DES_BLOCK_SIZE);
+        for(int i = 0; i < 64; i++) {
+                new->bits[permutation[i]] = block->bits[i];
+        }
+        return new;
+}
+
 // See DES.h for details
 BinStr DESencrypt(BinStr msg, BinStr key) {
-	assert(msg != NULL && key != NULL && key->length == 64);
+	assert(msg != NULL && key != NULL && key->length == DES_KEY_SIZE);
 	// TODO: Encrypt the message using DES
 	return msg;
 }
 
 // See DES.h for details
 BinStr DESdecrypt(BinStr cip, BinStr key) {
-	assert(cip != NULL && key != NULL && cip->length == key->length);
+	assert(cip != NULL && key != NULL && key->length == DES_KEY_SIZE);
 	// TODO: Decrypt the message using DES
 	return cip;
 }
--- Ciphers/DES.h
+++ Ciphers/DES.h
@@ -6,12 +6,15 @@
 
 #include "../Structures/BinStr.h"
 
+#define DES_KEY_SIZE 64
+#define DES_BLOCK_SIZE 64
+
 // DESencrypt(msg, key) encrypts the given message using the given key via the DES.
-// requires: msg and key are valid BinStr and key->length == 64
+// requires: msg and key are valid BinStr and key->length == DES_KEY_SIZE
 BinStr DESencrypt(BinStr msg, BinStr key);
 
 // DESdecrypt(cip, key) decrypts the given cipher using the given key via the DES.
-// requires: cip and key are valid BinStr and key->length == 64
+// requires: cip and key are valid BinStr and key->length == DES_KEY_SIZE
 BinStr DESdecrypt(BinStr cip, BinStr key);
 
 #endif
