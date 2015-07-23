36c7bd0c90266e670843fc07f3de73280dfa6ac4
Xenonstory <simon.j.hajjar@gmail.com>
2015-07-22 20:35:11 -0400

    added IV parameter for block ciphers, started CBC encrypt/decrypt functions

--- Generics/BlockCipher.c
+++ Generics/BlockCipher.c
@@ -6,10 +6,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+// CBCencrypt(msg, IV, cipher) encrypts the given message using the given
+//   cipher and IV, using CBC mode.
+// requires: msg and IV are valid BinStrs, cipher is a block cipher
+// effects: allocates memory to a new BinStr
+BinStr CBCencrypt(BinStr msg, BinStr IV, BlockCipher cipher) {
+    assert(msg != NULL && IV != NULL && cipher != NULL &&
+           msg->length % cipher->blockSize == 0 &&
+           IV->length % cipher->blockSize == 0);
+    // TODO: Actually encrypt
+    return msg;
+}
+
+// CBCdecrypt(cip, IV, cipher) decrypts the given message using the given
+//   cipher and IV, using CBC mode.
+// requires: cip and IV are valid BinStrs, cipher is a block cipher
+// effects: allocates memory to a new BinStr
+BinStr CBCdecrypt(BinStr cip, BinStr IV, BlockCipher cipher) {
+    assert(cip != NULL && IV != NULL && cipher != NULL &&
+           cip->length % cipher->blockSize == 0 &&
+           IV->length % cipher->blockSize == 0);
+    // TODO: Actually decrypt
+    return cip;
+}
 
 // ECBencrypt(msg, cipher) encrypts the given message using an  
 //   ECB mode block cipher and the given key                                    
 // requires: msg is a valid BinStr, cipher is a block cipher            
+// effects: allocates memory to a new BinStr
 BinStr ECBencrypt(BinStr msg, BlockCipher cipher) {                                              
     assert(msg != NULL && cipher != NULL && 
            msg->length % cipher->blockSize == 0);                                       
@@ -26,6 +50,7 @@ BinStr ECBencrypt(BinStr msg, BlockCipher cipher) {
 // ECBdecrypt(cip, cipher) decrypts the given message using an  
 //   ECB mode block cipher and the given key                                    
 // requires: cip is a valid BinStr, cipher is a block cipher            
+// effects: allocates memory to a new BinStr
 BinStr ECBdecrypt(BinStr cip, BlockCipher cipher) {
     assert(cip != NULL && cipher != NULL &&
            cip->length % cipher->blockSize == 0);
@@ -40,20 +65,24 @@ BinStr ECBdecrypt(BinStr cip, BlockCipher cipher) {
 }
 
 // See BlockCipher.h for details
-BinStr BlockEncrypt(BinStr msg, BlockCipher cipher) {
+BinStr BlockEncrypt(BinStr msg, BinStr IV, BlockCipher cipher) {
     assert(msg != NULL && cipher != NULL);
     if(strcmp(cipher->encryptionMode, "ECB") == 0) {
         return ECBencrypt(msg, cipher);
+    } else if (strcmp(cipher->encryptionMode, "CBC") == 0) {
+        return CBCEncrypt(msg, IV, cipher);
     } else {
         return NULL;
     }
 }
 
 // See BlockCipher.h for details
-BinStr BlockDecrypt(BinStr cip, BlockCipher cipher) {
+BinStr BlockDecrypt(BinStr cip, BinStr IV, BlockCipher cipher) {
     assert(cip != NULL && cipher != NULL);
     if(strcmp(cipher->encryptionMode, "ECB") == 0) {
         return ECBdecrypt(cip, cipher);
+    } else if (strcmp(cipher->encryptionMode, "CBC") == 0) {
+        return CBCdecrypt(cip, IV, cipher);
     } else {
         return NULL;
     }
--- Generics/BlockCipher.h
+++ Generics/BlockCipher.h
@@ -19,15 +19,17 @@ typedef struct blockcipher *BlockCipher;
 // BlockEncrypt(msg, cipher) encrypts the given message using the given block
 //   cipher, with pre-configured settings. Returns NULL if an invalid mode was
 //   given.
-// requires: msg != NULL and cipher != NULL
+// requires: msg != NULL and cipher != NULL and IV != NULL (unless on a mode
+//           without an IV).
 // effects: allocates memory to a cipher text
-BinStr BlockEncrypt(BinStr msg, BlockCipher cipher);
+BinStr BlockEncrypt(BinStr msg, BinStr IV, BlockCipher cipher);
 
 // BlockDecrypt(cip, cipher) decrypts the given ciphertext using the given
 //   block cipher, with pre-configured settings. Returns NULL If an invalid
 //   mode was given.
-// requires: cip != NULL and cipher != NULL
+// requires: cip != NULL and cipher != NULL and IV != NULL (unless on a mode
+//           without an IV)
 // effects: allocates memory to a message
-BinStr BlockDecrypt(BinStr cip, BlockCipher cipher);
+BinStr BlockDecrypt(BinStr cip, BinStr IV, BlockCipher cipher);
 
 #endif
