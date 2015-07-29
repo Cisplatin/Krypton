d819d20ad49adc0c3cbadeff9884da9bed0e4939
Simon Hajjar <simon.j.hajjar@gmail.com>
2015-07-29 13:28:19 -0400

    created 3DES header

--- Ciphers/DES.h
+++ Ciphers/DES.h
@@ -7,13 +7,13 @@
 #include "../Structures/BinStr.h"
 #include "../Generics/BlockCipher.h"
 
-// DESinitialize(key, mode) returns an instance of a block cipher ready for
+// DES_initialize(key, mode) returns an instance of a block cipher ready for
 //   decryption and encryption using the given key and mode
 // requires: key != NULL, mode is a valid mode (ECB, etc.)
 // effects: allocates memory to a new DES cipher
 BlockCipher DES_initialize(BinStr key, char* mode);
 
-// DESdestroy(cipher) destroys the given instance of the DES block cipher
+// DES_destroy(cipher) destroys the given instance of the DES block cipher
 // requires: cipher != NULL and cipher is a DES block cipher
 // effects: frees memory used by the cipher
 void DES_destroy(BlockCipher DES);
