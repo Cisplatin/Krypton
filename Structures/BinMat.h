44680b2bf3e9bd0566d521715c6345f85f3f338c
Simon Hajjar <simon.j.hajjar@gmail.com>
2015-07-30 11:10:13 -0400

    started BinMat header

---
+++ Structures/BinMat.h
@@ -0,0 +1,21 @@
+/* BinMat.h - Written by Xenonstory on July 30, 2015  *
+ * A structure used to store binary matrices          */
+
+#ifndef BINMAT_H
+#define BINMAT_H
+
+#include <stdbool.h>
+#include "BinStr.h"
+
+typedef struct binstr **BinStr;
+
+/*
+// reversePermutate(str, order, len, offset) reverse permutates the given BinStr
+//   with similar attributes to the permutate() function.
+// requires: str is a valid BinStr, order is a valid array of ints, len >= 0,
+//           and offset >= 0
+BinStr reversePermutate(BinStr str, int *order, int len, int offset);
+*/
+
+#endif
+
