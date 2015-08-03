62331ab7cd650505d1e7dbd2c1ebf4e13d206912
Xenonstory <simon.j.hajjar@gmail.com>
2015-08-03 14:24:11 -0400

    worked a bit more on MDC-2 hash

--- Hashes/MDC2.c
+++ Hashes/MDC2.c
@@ -2,15 +2,24 @@
    An implementation of the MDC-2 cryptographic hash */
 
 #include "MDC2.h"
+#include "../Structures/BinStr.h"
+#include "../Ciphers/DES.h"
 #include <assert.h>
 
 const int MDC2_BLOCK_SIZE = 64;
 
 // See MDC2.h for details
 BinStr MDC2(BinStr str, BinStr IV_1, BinStr IV_2) {
-    assert(str->length % MDC2_BLOCK_SIZE == 0 &&
+    assert(str != NULL && IV_1 != NULL && IV_2 != NULL &&
+           str->length % MDC2_BLOCK_SIZE == 0 &&
            IV_1->length == MDC2_BLOCK_SIZE &&
            IV_2->length == MDC2_BLOCK_SIZE);
-    // TODO
-    return str;
+    BinStr H_0 = copy(IV_1);
+    BinStr H_1 = copy(IV_2);
+
+    // TODO: Finish hashing function
+
+    H_0 = set(H_0, append(H_0, H_1));
+    destroy_BinStr(H_1);
+    return H_0;
 }
--- Hashes/MDC2.h
+++ Hashes/MDC2.h
@@ -4,6 +4,8 @@
 #ifndef MDC2_H
 #define MDC2_H
 
+#include "../Structures/BinStr.h"
+
 // MDC2(str, IV_1, IV_2) returns the MDC-2 hash of the given string
 // requires: string has a bit length that is a multiple of 64, and
 //           IV_1->length == IV_2->length == 64
