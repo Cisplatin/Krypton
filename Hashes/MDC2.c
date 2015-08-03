9fd72e18d964f0e51c0c82f0794f4d2164a084a1
Xenonstory <simon.j.hajjar@gmail.com>
2015-08-03 17:18:47 -0400

    added g-functions for MDC-2 hash

--- Hashes/MDC2.c
+++ Hashes/MDC2.c
@@ -8,6 +8,44 @@
 
 const int MDC2_BLOCK_SIZE = 64;
 
+// generateG1(H) returns the result of the first g function for MDC-2
+// requires: H is a valid BinStr and H->length == MDC2_BLOCK_SIZE
+// effects: allocates memory to a new BinStr
+BinStr generateG1(BinStr H) {
+    assert(H != NULL && H->length == MDC2_BLOCK_LENGTH);
+    BinStr key = copy(H);
+    key->bits[1] = 1;
+    key->bits[2] = 0;
+    for(int i = BITS_PER_BYTE - 1; i < H->length; i += BITS_PER_BYTE) {
+        BinStr seg = snip(key, i, i + BITS_PER_BYTE - 1);
+        if(parity(seg)) { 
+            int verify_bit = i + BITS_PER_BYTE - 1;
+            key->bits[verify_bit] = !key->bits[verify_bit];
+        }
+        destroy_BinStr(seg);
+    }
+    return key
+}
+
+// generateG2(H) returns the result of the second g function for MDC-2
+// requires: H is a valid BinStr and H->length == MDC2_BLOCK_SIZE
+// effects: allocates memory to a new BinStr
+BinStr generateG2(BinStr H) {
+    assert(H != NULL && H->length == MDC2_BLOCK_LENGTH);
+    BinStr key = copy(H);
+    key->bits[1] = 0;
+    key->bits[2] = 1;
+    for(int i = BITS_PER_BYTE - 1; i < H->length; i += BITS_PER_BYTE) {
+        BinStr seg = snip(key, i, i + BITS_PER_BYTE - 1);
+        if(parity(seg)) { 
+            int verify_bit = i + BITS_PER_BYTE - 1;
+            key->bits[verify_bit] = !key->bits[verify_bit];
+        }
+        destroy_BinStr(seg);
+    }
+    return key
+}
+
 // See MDC2.h for details
 BinStr MDC2(BinStr str, BinStr IV_1, BinStr IV_2) {
     assert(str != NULL && IV_1 != NULL && IV_2 != NULL &&
