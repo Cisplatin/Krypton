c351d9c69d8cb9101d54814d75f77bf7eaeb1f8e
Simon Hajjar <simon.j.hajjar@gmail.com>
2015-08-04 13:54:53 -0400

    removed HMAC content (not prepared for yet, need to create hash generic)

--- MACs/HMAC.h
+++
@@ -1,9 +0,0 @@
-/* HMAC.h - Created by Xenonstory on August 4th, 2015
-   HMAC is an implementation of the HMAC authentication code */
-
-#include "../Structures/BinStr.h"
-
-// HMAC(msg, key, hash) returns the HMAC tag for the given msg using the given
-//   key, using the hash function supplied.
-// requires: msg and key are valid BinStrs, hash is a cryptographic hash function
-BinStr HMAC(BinStr msg, BinStr key, BinStr (*hash)(BinStr)); 
