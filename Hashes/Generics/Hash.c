f505b5029606e61391ebc33f3ada274833093eeb
Simon Hajjar <simon.j.hajjar@gmail.com>
2015-08-04 16:45:19 -0400

    Started writing padding function

--- Hashes/Generics/Hash.c
+++ Hashes/Generics/Hash.c
@@ -5,9 +5,17 @@
 #include "../../Structures/BinStr.h"
 #include <assert.h>
 
+// padding(str, blockSize) pads the given string with a 1 followed by an
+//   amount of zeroes so that the resulting text is a multiple of the given
+//   block size
+// requires: str is a valid BinStr, blockSize > 0
+void padding(BinStr str, int blockSize) {
+    assert(str != NULL && blockSize > 0);
+    // TODO: Finish padding function
+}
+
 // See Hash.h for details
 BinStr hashValue(BinStr str, Hash alg) {
     assert(str->length % alg->blockSize == 0);
     return alg->hashFunc(str);
 }
-
