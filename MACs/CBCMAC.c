f964ebbf16ae49c0006eb6dd190af0df67ac59d6
Simon Hajjar <simon.j.hajjar@gmail.com>
2015-08-14 17:14:24 -0400

    fixed some syntax and segfaults

--- MACs/CBCMAC.c
+++ MACs/CBCMAC.c
@@ -6,17 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+// See CBCMAC.h for details
 BinStr CBCMAC(BinStr str, BlockCipher cipher) {
     assert(str != NULL && cipher != NULL && 
            strcmp(cipher->encryptionMode, "ECB") == 0);
-    BinStr padded = copy(str);
+    BinStr padded = copyStr(str);
     paddingMethod2(padded, cipher->blockSize);
     BinStr tag = NULL;
     for(int i = 0; i + cipher->blockSize < str->length
                  ; i += cipher->blockSize) {
-        BinStr buffer = snip(padded, i, i + cipher->blockSize);
+        BinStr buffer = snip(padded, i, i + cipher->blockSize - 1);
+        // Add an exception for the first run through
         if(tag != NULL) tag = set(tag, XOR(tag, buffer));
-        else            tag = buffer;
+        else            tag = copyStr(buffer);
         tag = set(tag, BlockEncrypt(tag, NULL, cipher));
         destroy_BinStr(buffer);
     }
