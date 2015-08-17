a5dda70244a90767a0cba0bb40b1f466293d699d
Simon Hajjar <simon.j.hajjar@gmail.com>
2015-08-17 16:33:48 -0400

    wrote MD4 padding step

--- Hashes/MD4.c
+++ Hashes/MD4.c
@@ -13,8 +13,21 @@ const int MD4_BLOCK_SIZE = 1;
 // requires: str is a valid BinStr
 // effects: allocates new memory to the resulting BinStr
 BinStr MD4func(BinStr str) {
-    // TODO
-    return str;
+    assert(str != NULL);
+    
+    // Pad the until it is congruent to 448 mod 512
+    BinStr tag = copy(str);
+    BinStr to_app = str_to_BinStr('1', 1);
+    str = set(str, append(str, to_app));
+    to_app = set(to_app, str_to_BinStr('0', 1));
+    while(str->length % 512 != 448) {
+        str = set(str, append(str, to_app));
+    }
+    destroy_BinStr(to_app);
+
+    // TODO: Finish MD4 algorithm
+
+    return tag;
 }
 
 // See MD4.h for details
